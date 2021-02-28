71e56a0d14dc40fbe56445977d9a19d140c6b8d4
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-02-28 13:08:00 -0500

    dfgdsfgsdfg

--- resources/views/landing/contactos.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Contactos</h1>
-<a href="{{ route('Landing.index') }}">index</a>
-<a href="{{ route('Landing.domicilios') }}">domicilios</a>
-<a href="{{ route('Landing.acercade') }}">acercade</a>
